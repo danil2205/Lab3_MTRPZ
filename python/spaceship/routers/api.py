8e6124146c51db829702032017d35cfccdbbd062
danil2205 <04danil@gmail.com>
2023-03-30 00:00:03 +0300

    added: new endpoint api.py, versions to each dependencies

--- python/spaceship/routers/api.py
+++ python/spaceship/routers/api.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+import numpy as np
 
 router = APIRouter()
 
@@ -6,3 +7,10 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.get('/multiply_matrixcs')
+def multiply_matrixcs() -> dict:
+    matrix_a = np.random.rand(10, 10).tolist()
+    matrix_b = np.random.rand(10, 10).tolist()
+    product = np.matmul(matrix_a, matrix_b).tolist()
+    return {'matrix_a': matrix_a, 'matrix_b': matrix_b, 'product': product}
